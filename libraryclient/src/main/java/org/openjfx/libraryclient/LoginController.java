6eebf486e6e87aef996f7d23834043f3b2a1ca4a
Uri Nights <Uri Nights@UriWork>
2020-11-18 20:52:17 +0100

    -

---
+++ libraryclient/src/main/java/org/openjfx/libraryclient/LoginController.java
@@ -0,0 +1,12 @@
+package org.openjfx.libraryclient;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
